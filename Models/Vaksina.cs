2458c5ef26c8cc4b0f9bfd6605e8c27741c47958
JetmirD <81836743+JetmirD@users.noreply.github.com>
2022-05-23 04:13:40 +0200

    Add files via upload

---
+++ Models/Vaksina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Vaksinat
+    {
+        public int Id { get; set; }
+        public string Emri { get; set; }
+        public int NumriIVaksinuarit { get; set; }
+        public String Vendlindja { get; set; }
+        public String LlojiVaksines { get; set; }
+
+        public String DataVaksinimit { get; set; }
+        public String Mjeku { get; set; }
+        public int NrIDozave { get; set; }
+
+    }
+}
